de1f929d61d2b877f9de8bffa52c4d748f1a3f27
NahidulNoman <nahidulna5@gmail.com>
2022-12-22 22:29:09 +0600

    folder structure added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './routes/router';
 
 function App() {
   return (
-    <div className="App">
-      <button className="btn">Button</button>
-<button className="btn btn-primary">Button</button>
-<button className="btn btn-secondary">Button</button>
-<button className="btn btn-accent">Button</button>
-<button className="btn btn-ghost">Button</button>
-<button className="btn btn-link">Button</button>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
