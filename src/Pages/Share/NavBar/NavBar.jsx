4de72da77eee58bc3deff526caca25cc313fece5
NahidulNoman <nahidulna5@gmail.com>
2022-12-22 22:39:08 +0600

    navbar implements

--- src/Pages/Share/NavBar/NavBar.jsx
+++ src/Pages/Share/NavBar/NavBar.jsx
@@ -1,11 +1,99 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <div>
-            
+
+  const menuItems = (
+    <>
+      <li className="font-semibold ">
+        <Link to="/home" className="rounded-lg">
+          Home
+        </Link>
+      </li>
+      <li className="font-semibold">
+        <Link to="/causes" className="rounded-lg">
+        Causes
+        </Link>
+      </li>
+      <li className="font-semibold">
+        <Link to="/event" className="rounded-lg">
+        Event
+        </Link>
+      </li>
+      <li className="font-semibold">
+        <Link to="/blog" className="rounded-lg">
+          Blog
+        </Link>
+      </li>
+      <li className="font-semibold">
+        <Link to="/about" className="rounded-lg">
+          About
+        </Link>
+      </li>
+      <li className="font-semibold">
+        <Link to="/contact" className="rounded-lg">
+          Contact
+        </Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-white shadow-sm rounded-lg flex justify-between">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={1}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-white rounded-box w-52"
+          >
+            {menuItems}
+          </ul>
         </div>
-    );
+        <Link to="/" className="btn btn-ghost text-accent normal-case text-xl">
+          Next Car For You
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+      </div>
+      <label
+        htmlFor="dashboard-drawer"
+        tabIndex={2}
+        className="btn btn-ghost lg:hidden"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16"
+          />
+        </svg>
+      </label>
+    </div>
+  );
 };
 
 export default NavBar;
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import About from "../Pages/About/About";
+import Blog from "../Pages/Blog/Blog";
+import Causes from "../Pages/Causes/Causes";
+import Contact from "../Pages/Contact/Contact";
+import Event from "../Pages/Event/Event";
 import Home from "../Pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
@@ -10,7 +15,27 @@ export const router = createBrowserRouter([
             {
                 path : '/home',
                 element : <Home></Home>
-            }
+            },
+            {
+                path : '/causes',
+                element : <Causes></Causes>
+            },
+            {
+                path : '/blog',
+                element : <Blog></Blog>
+            },
+            {
+                path : '/about',
+                element : <About></About>
+            },
+            {
+                path : '/contact',
+                element : <Contact></Contact>
+            },
+            {
+                path : '/event',
+                element : <Event></Event>
+            },
         ]
     }
 ])
