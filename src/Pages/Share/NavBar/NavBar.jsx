d4b28104a81238c0fa798b85cfdb1ffb9cb49aa2
NahidulNoman <nahidulna5@gmail.com>
2022-12-22 22:50:36 +0600

    navbar modify

--- src/Pages/Share/NavBar/NavBar.jsx
+++ src/Pages/Share/NavBar/NavBar.jsx
@@ -1,37 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from '../../../assets/seelifeLogo.png';
 
 const NavBar = () => {
 
   const menuItems = (
     <>
       <li className="font-semibold ">
-        <Link to="/home" className="rounded-lg">
+        <Link to="/home" className="rounded-lg text-white">
           Home
         </Link>
       </li>
       <li className="font-semibold">
-        <Link to="/causes" className="rounded-lg">
+        <Link to="/causes" className="rounded-lg text-white">
         Causes
         </Link>
       </li>
       <li className="font-semibold">
-        <Link to="/event" className="rounded-lg">
+        <Link to="/event" className="rounded-lg text-white">
         Event
         </Link>
       </li>
       <li className="font-semibold">
-        <Link to="/blog" className="rounded-lg">
+        <Link to="/blog" className="rounded-lg text-white">
           Blog
         </Link>
       </li>
       <li className="font-semibold">
-        <Link to="/about" className="rounded-lg">
+        <Link to="/about" className="rounded-lg text-white">
           About
         </Link>
       </li>
       <li className="font-semibold">
-        <Link to="/contact" className="rounded-lg">
+        <Link to="/contact" className="rounded-lg text-white">
           Contact
         </Link>
       </li>
@@ -39,7 +40,7 @@ const NavBar = () => {
   );
 
   return (
-    <div className="navbar bg-white shadow-sm rounded-lg flex justify-between">
+    <div className="navbar bg-green-400 shadow-sm flex justify-between">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -60,13 +61,13 @@ const NavBar = () => {
           </label>
           <ul
             tabIndex={1}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-white rounded-box w-52"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-green-400 rounded-box w-52"
           >
             {menuItems}
           </ul>
         </div>
-        <Link to="/" className="btn btn-ghost text-accent normal-case text-xl">
-          Next Car For You
+        <Link to="/" className="text-accent normal-case text-xl">
+          <img src={logo} alt="" />
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
