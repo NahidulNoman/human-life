feef10c1801efc317d5edae60f6a31bb88233691
NahidulNoman <nahidulna5@gmail.com>
2022-12-22 23:48:49 +0600

    causes data loaded

--- src/Pages/Home/CausesHome/CausesHome.jsx
+++ src/Pages/Home/CausesHome/CausesHome.jsx
@@ -26,7 +26,21 @@ const CausesHome = () => {
     ]
     return (
         <div>
-            
+            <h2>Our Major Causes</h2>
+            <p>Creepeth called face upon face yielding midst is after moveth</p>
+            <div>
+                {
+                    donateData.map(data => <div key={data.id} className="card w-96 bg-base-100 shadow-xl">
+                        <h2 className="card-title">{data.name}</h2>
+                    <figure className="px-10 pt-10">
+                      <img src={data.image} alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                      <p>{data.text}</p>
+                    </div>
+                  </div>)
+                }
+            </div>
         </div>
     );
 };
