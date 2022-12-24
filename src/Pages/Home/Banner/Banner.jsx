74abf8310352e2248bebf55b4928cc70c367f2ad
NahidulNoman <nahidulna5@gmail.com>
2022-12-24 20:49:04 +0600

    banner page midify

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -5,19 +5,19 @@ const Banner = () => {
   return (
     // <div className="bannerContainer">
     //   <div className=" text-white">
-        // <h2 className="sm:text-3xl lg:text-6xl text-center mb-3 text-green-600">GIVE A HAND</h2>
-        // <h3 className="sm:text-2xl lg:text-4xl text-center mb-3 font-bold">
-        //   TO MAKE THE BETTER WORLD
-        // </h3>
-        // <p className="text-md lg:text-center opacity-75">
-        //   That don't lights. Blessed land spirit creature divide our made two
-        //   itself upon you'll dominion waters <br /> man second good you they're divided
-        //   upon winged were replenish night
-        // </p>
-        // <div className="mt-7 text-center">
-        // <button className="btn btn-outline btn-warning mr-6">Donate Now</button>
-        // <button className="btn btn-outline btn-warning">See Causes</button>
-        // </div>
+    // <h2 className="sm:text-3xl lg:text-6xl text-center mb-3 text-green-600">GIVE A HAND</h2>
+    // <h3 className="sm:text-2xl lg:text-4xl text-center mb-3 font-bold">
+    //   TO MAKE THE BETTER WORLD
+    // </h3>
+    // <p className="text-md lg:text-center opacity-75">
+    //   That don't lights. Blessed land spirit creature divide our made two
+    //   itself upon you'll dominion waters <br /> man second good you they're divided
+    //   upon winged were replenish night
+    // </p>
+    // <div className="mt-7 text-center">
+    // <button className="btn btn-outline btn-warning mr-6">Donate Now</button>
+    // <button className="btn btn-outline btn-warning">See Causes</button>
+    // </div>
     //   </div>
     // </div>
     <div
@@ -28,17 +28,22 @@ const Banner = () => {
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
           {/* <h1 className="mb-5 text-5xl font-bold">Hello there</h1> */}
-          <h2 className="sm:text-3xl lg:text-6xl text-center mb-3 text-green-600">GIVE A HAND</h2>
-        <h3 className="sm:text-2xl lg:text-3xl text-center mb-3 font-bold">
-          TO MAKE THE BETTER WORLD
-        </h3>
+          <h2 className="sm:text-3xl lg:text-6xl text-center mb-3 text-green-600">
+            GIVE A HAND
+          </h2>
+          <h3 className="sm:text-2xl lg:text-3xl text-center mb-3 font-bold">
+            TO MAKE THE BETTER WORLD
+          </h3>
           <p className="mb-5">
-          That don't lights. Blessed land spirit creature divide our made two itself upon you'll dominion waters.
+            That don't lights. Blessed land spirit creature divide our made two
+            itself upon you'll dominion waters.
           </p>
           <div className="mt-7 text-center">
-        <button className="btn btn-outline btn-warning mr-6">Donate Now</button>
-        <button className="btn btn-outline btn-warning">See Causes</button>
-        </div>
+            <button className="btn btn-outline btn-warning mr-6">
+              Donate Now
+            </button>
+            <button className="btn btn-outline btn-warning">See Causes</button>
+          </div>
         </div>
       </div>
     </div>
