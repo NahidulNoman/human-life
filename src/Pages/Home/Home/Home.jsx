a735ca0e1d9b521222a262eb81d817e73cb3ccb2
NahidulNoman <nahidulna5@gmail.com>
2022-12-25 22:11:38 +0600

    home page inplements and modify

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import CausesHome from '../CausesHome/CausesHome';
-import FeaturedCausesHome from '../FeaturedCausesHome/FeaturedCausesHome';
-import TeamWork from '../TeamWork/TeamWork';
+import React from "react";
+import Banner from "../Banner/Banner";
+import CausesHome from "../CausesHome/CausesHome";
+import FeaturedCausesHome from "../FeaturedCausesHome/FeaturedCausesHome";
+import TeamWork from "../TeamWork/TeamWork";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <CausesHome></CausesHome>
-            <TeamWork></TeamWork>
-            <FeaturedCausesHome></FeaturedCausesHome>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <CausesHome></CausesHome>
+      <TeamWork></TeamWork>
+      <FeaturedCausesHome></FeaturedCausesHome>
+    </div>
+  );
 };
 
 export default Home;
